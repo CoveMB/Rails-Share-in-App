73b0895ea3fe32464465f68df1816edf43cd0540
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-24 17:09:15 -0400

    fix syntax error

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,5 @@
 class Interest < ApplicationRecord
   belongs_to :interest_category
   has_many :user_interests
-  has_many :users, throught: :user_interests
+  has_many :users, through: :user_interests
 end

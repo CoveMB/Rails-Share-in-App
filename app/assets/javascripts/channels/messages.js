f8c32869e0038822a7c372df53a497f3e390a3cd
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-29 08:16:17 -0400

    message layout improve

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -9,6 +9,10 @@ function createMessageChannel() {
         },
         renderMessage: function(data) {
           var user_status = (window.username == data.user_name) ? "current" : "other" ;
+          if (user_status == "other"){
+            document.getElementById("notification-area").insertAdjacentHTML("beforeend", '<span class="badge badge-light">New</span>');
+            document.getElementById("user-" + data.user_id).style.fontWeight = "bold";
+          }
     return '<div class="' + user_status + '-user-custom-row"><a href="/users/' + data.user_id + '"><img class="avatar" src="' + data.user_avatar + '"></a><p class="' + user_status + '-user-message">' + data.message + '</p><p class="time-indicator-message">just now</p></div>';
   },
       });

05097d0e6305304f1d3c10f2a7d70619c97663d2
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-30 13:50:58 -0400

    strong message select

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -10,14 +10,14 @@ function createMessageChannel() {
         renderMessage: function(data) {
           var user_status = (window.username == data.user_name) ? "current" : "other" ;
           setTimeout(function() { var objDiv = document.getElementById("messages").parentElement;
-          objDiv.scrollTop = objDiv.scrollHeight;}, 8);
+          objDiv.scrollTop = objDiv.scrollHeight;}, 1);
 
           if (user_status == "other"){
             document.getElementById("notification-area").insertAdjacentHTML("beforeend", '<span class="badge badge-light">New</span>');
             document.getElementById("user-" + data.user_id).style.fontWeight = "bold";
             document.querySelector("body").insertAdjacentHTML("beforeend", '<div class="alert alert-info alert-dismissible fade show m-1" role="alert">' + data.user_name + ' sent you new a message <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
           }
-    return '<div class="' + user_status + '-user-custom-row animated fadeIn delay-5"><a href="/users/' + data.user_id + '"><img class="avatar" src="' + data.user_avatar + '"></a><p class="' + user_status + '-user-message">' + data.message + '</p><p class="time-indicator-message">just now</p></div>';
+    return '<div class="' + user_status + '-user-custom-row animated fadeIn delay-6"><a href="/users/' + data.user_id + '"><img class="avatar" src="' + data.user_avatar + '"></a><p class="' + user_status + '-user-message">' + data.message + '</p><p class="time-indicator-message">just now</p></div>';
   },
       });
 return App.messages;

2edd0602f4e0130f0f274427419729d965bd8f92
Benjamin Marquis <bmarquiscom@gmail.com>
2019-09-03 11:16:11 -0400

    fix

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,4 +1,5 @@
 function createMessageChannel() {
+  AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
   App.messages = App.cable.subscriptions.create({
         channel: 'MessagesChannel', chat_id: parseInt($("#message_chat_id").val())
         },
@@ -37,7 +38,7 @@ function createMessageChannel() {
         buildChatBox: function(data){
           // Insert preview and message box
           document.querySelector(".message-little-bar").insertAdjacentHTML("beforeend",
-          '<div class="message-little-preview" id="messages-preview-'+ data.chat_id +'" style="display: block;"><h3>'+ data.user_name +'<span id="message-preview-notification" class="badge badge-light" style="display: block;">New</span></h3></div>' + '<div class="message-little-box scrollbar-custom" id="message-box-'+ data.chat_id +'" style="display: none;"><div class="messages-actions"><span class="reduce-message">-</span><span class="closing-message">X</span></div><div class="messages" id="messages-'+ data.chat_id +'"></div><form class="new_message" id="new_message" action="/messages" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="Rg7qOt6u1yvmP9eAS7MNYM9y2qzBsTsIbAssuXPoG04UauoiNrTdEKr9KQkoHDBRa+6G8CpLSHNvrLCyCTyC8A==" /><textarea data-textarea="message" class="form-control" name="message[content]" id="message_content"></textarea><input value="'+ data.chat_id +'" type="hidden" name="message[chat_id]" id="message_chat_id" /><input value="'+ data.user_id +'" type="hidden" name="message[other_user]" id="message_other_user" /></form></div>'
+          '<div class="message-little-preview" id="messages-preview-'+ data.chat_id +'" style="display: block;"><h3>'+ data.user_name +'<span id="message-preview-notification" class="badge badge-light" style="display: block;">New</span></h3></div>' + '<div class="message-little-box scrollbar-custom" id="message-box-'+ data.chat_id +'" style="display: none;"><div class="messages-actions"><span class="reduce-message">-</span><span class="closing-message">X</span></div><div class="messages" id="messages-'+ data.chat_id +'"></div><form class="new_message" id="new_message" action="/messages" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="' + AUTH_TOKEN + '" /><textarea data-textarea="message" class="form-control" name="message[content]" id="message_content"></textarea><input value="'+ data.chat_id +'" type="hidden" name="message[chat_id]" id="message_chat_id" /><input value="'+ data.user_id +'" type="hidden" name="message[other_user]" id="message_other_user" /></form></div>'
          );
        },
        addStyling: function(){

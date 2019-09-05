23e65ce4533a4c3a609aa17a92b9f9f96d422f59
Benjamin Marquis <bmarquiscom@gmail.com>
2019-09-05 18:48:41 -0400

    fix css

--- db/seeds/j_extra_modeling.rb
+++ db/seeds/j_extra_modeling.rb
@@ -1,3 +1,6 @@
+kristyna = User.find_by_name("Kristyna")
+kristyna.events = []
+
 yatai = Event.find_by_name("YATAI - Japan Street Food Celebration")
 yatai.start_date = DateTime.parse("7th september 2019")
 yatai.end_date = DateTime.parse("8th september 2019")
@@ -12,5 +15,10 @@ alexandraplatz.end_date = DateTime.parse("15th september 2019")
 alexandraplatz.users = []
 alexandraplatz.users << User.find_by_name("Kristyna")
 alexandraplatz.save!
+
+kristyna.events <<  Event.find_by_name("THIERRY MUGLER - Couturissime")
+kristyna <<  Event.find_by_name("ENVIRONMENT MUSEUM")
+kristyna <<  Event.find_by_name("FIESTA OLYMPICA")
+kristyna.save!
 
 p "Seed modeling done!"

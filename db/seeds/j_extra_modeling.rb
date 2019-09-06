4c1c24518b0687db04322ef0600ee690f8ed0a1f
Benjamin Marquis <bmarquiscom@gmail.com>
2019-09-06 14:06:42 -0400

    seed

--- db/seeds/j_extra_modeling.rb
+++ db/seeds/j_extra_modeling.rb
@@ -14,6 +14,7 @@ alexandraplatz.start_date = DateTime.parse("14th september 2019")
 alexandraplatz.end_date = DateTime.parse("15th september 2019")
 alexandraplatz.users = []
 alexandraplatz.users << User.find_by_name("Kristyna")
+alexandraplatz.description = "There’s something bittersweet about witnessing the development of an emerging neighbourhood. Too often, we’re only privy to our city’s secret spots once they’re out of the bag and crowded, or we want to hold on to them before they are. Mile Ex is currently in the midst of its transformation, and still has that twinge of excitement and mystery to it."
 alexandraplatz.save!
 
 kristyna.events <<  Event.find_by_name("THIERRY MUGLER - Couturissime")
